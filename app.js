81b3ecefe5a7ea2e7929c3438934bf241f749cf5
Erfanfooladii <arfannnfooladi@gmail.com>
2025-07-22 17:34:30 +0330

    fix: update response message in app.js to return user details

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("سلام بر استاد سالکی این یک پیام ابری هست!");
+  res.send({ name: "erfan", age: 22 });
 });
 
 app.listen(PORT, () => {
